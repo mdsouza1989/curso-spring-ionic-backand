dd6e8873917676abbeffe4a8299620fe631479d0
mdsouza1989 <marcelodisse1989@gmail.com>
2018-12-22 09:37:45 -0200

    metodo CRUD e lista DTO

--- src/main/java/com/marcelodias/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/marcelodias/cursomc/resources/CategoriaResource.java
@@ -1,13 +1,20 @@
 package com.marcelodias.cursomc.resources;
 
+import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.marcelodias.cursomc.domain.Categoria;
+import com.marcelodias.cursomc.dto.CategoriaDTO;
 import com.marcelodias.cursomc.services.CategoriaService;
 
 @RestController
@@ -19,10 +26,45 @@ public class CategoriaResource {
 	
 	@RequestMapping(value="/{id}", method=RequestMethod.GET)
 	public ResponseEntity<?> find(@PathVariable Integer id) {
-		Categoria obj = service.buscar(id);
+		Categoria obj = service.find(id);
 			return ResponseEntity.ok().body(obj);
 					
 	}
+	
+	@RequestMapping(method=RequestMethod.POST)
+	public ResponseEntity<Void> insert(@RequestBody Categoria obj){
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+				.path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
+	}
+	
+	@RequestMapping(value="/{id}", method=RequestMethod.PUT)
+	public ResponseEntity<Void> update(@RequestBody Categoria obj, @PathVariable Integer id){
+		obj.setId(id);
+		obj = service.update(obj);
+		return ResponseEntity.noContent().build();
+	}
+	
+	@RequestMapping(value="/{id}", method=RequestMethod.DELETE)
+	public ResponseEntity<Void> delete(@PathVariable Integer id){
+		service.delete(id);
+		return ResponseEntity.noContent().build();
+	}
+	
+	//assim retorna uma lista de categorias com todos os produtos o CATEGORIA DTO 
+	//faz uma lista em outra lista para trazer somente a categoria ou seja informatica e escritorio.
+	
+	//@RequestMapping(method=RequestMethod.GET)
+	//public ResponseEntity<List<Categoria>> findAll() {
+	//	List<Categoria> list = service.findAll()
+	//		return ResponseEntity.ok().body(list);
+					
 
-
+	@RequestMapping(method=RequestMethod.GET)
+	public ResponseEntity<List<CategoriaDTO>> findAll() {
+		List<Categoria> list = service.findAll();
+		List<CategoriaDTO> listDto = list.stream().map(obj -> new CategoriaDTO(obj)).collect(Collectors.toList());
+			return ResponseEntity.ok().body(listDto);
+	}
 }
--- src/main/java/com/marcelodias/cursomc/services/CategoriaService.java
+++ src/main/java/com/marcelodias/cursomc/services/CategoriaService.java
@@ -1,10 +1,14 @@
 package com.marcelodias.cursomc.services;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.marcelodias.cursomc.domain.Categoria;
 import com.marcelodias.cursomc.repositories.CategoriaRepository;
+import com.marcelodias.cursomc.services.Exception.DataIntegrityException;
 import com.marcelodias.cursomc.services.Exception.ObjectNotFoundException;
 
 @Service
@@ -13,12 +17,37 @@ public class CategoriaService {
 	@Autowired
 	private CategoriaRepository repo;
 	
-	public Categoria buscar(Integer id) {
+	public Categoria find(Integer id) {
 		Categoria obj = repo.findOne(id);
 		if(obj == null) {
 			throw new ObjectNotFoundException("Objeto não encontrado! Id:" + id + ",Tipo:" +Categoria.class.getName());
 		}
 		return obj;
 	}
-
+	
+	public Categoria insert(Categoria obj) {
+		obj.setId(null);
+		return repo.save(obj);
+	}
+	
+	public Categoria update(Categoria obj) {
+		find(obj.getId());
+		return repo.save(obj);
+	}
+	
+	public void delete(Integer id) {
+		find(id);
+		try {
+		repo.delete(id);
+	}
+		catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possivel excluir categoria que possue produtos");
+		}
+	}
+	
+	public List<Categoria> findAll() {
+		return repo.findAll();
+		
+	}
 }
+	
